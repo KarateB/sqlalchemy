54d3f495dbd508e9830ac989e3918d1e9170e658
Berkay Karatay <65409456+KarateB@users.noreply.github.com>
2022-12-11 17:31:28 +0300

    add __repr

--- sqlalchemy-lib/sqlalchemy.components/BaseModuleMixins.py
+++ sqlalchemy-lib/sqlalchemy.components/BaseModuleMixins.py
@@ -3,10 +3,8 @@ import datetime
 from infos import url
 
 from sqlalchemy import Column, String, Integer, ForeignKey, Boolean, Table, create_engine, DateTime
-from sqlalchemy.orm import relationship, sessionmaker, backref
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.sql.sqltypes import TIMESTAMP
-from sqlalchemy.sql.expression import text
 from sqlalchemy_mixins import UserMixin, BaseMixin
 
 from datetime import datetime as dt
@@ -16,8 +14,12 @@ session = sessionmaker(bind=engine)()
 Base = declarative_base()
 
 
-class BaseModule(BaseMixin):
+class UserModule(UserMixin):
     ...
+
+
+
+
     # todo class BaseMixin will appear in all parent classes
     """ id_ = Column(Integer, primary_key=True, nullable=False)
     created_at = Column(DateTime, server_default=dt.utcnow(), nullable=False)
@@ -25,7 +27,7 @@ class BaseModule(BaseMixin):
     """
 
 
-class Port(BaseModule):
+class Port(BaseMixin, UserModule):
     __tablename__ = "posts"
 
     # todo class BaseMixin(Base) will appear in class Port(BaseMixin) even tho it is not indicated.
@@ -33,5 +35,27 @@ class Port(BaseModule):
 
     title = Column(String, nullable=False)
     content = Column(String, nullable=False)
-    published = Column(Boolean,  server_default=True, nullable=False)
+    published = Column(Boolean,  server_default="TRUE", nullable=False)
+
+    def __repr__(self):
+        _return = f' < \n' \
+                  f' id : {self.id}\n ' \
+                  f'created_at: {self.created_at}\n ' \
+                  f'updated_at: {self.updated_at}\n ' \
+                  f'title: {self.title}\n ' \
+                  f'content: {self.content}\n ' \
+                  f'published: {self.published}\n ' \
+                  f'>'
+        return _return
+
+
+port = Port()
+port.id = 1
+port.created_at = dt.utcnow()
+port.updated_at = dt.utcnow()
+port.published = True
+port.title = "New Title with a lot of content and fun."
+port.content = "A new content with a lot of content and fun."
+print(port)
+
 
